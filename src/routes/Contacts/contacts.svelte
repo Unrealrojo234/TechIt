814422189ad62fc361801bd29f01ade6903e7f11
Unrealrojo234 <otienotyan812@gmail.com>
2025-01-02 23:33:25 +0300

    Minor changes

--- src/routes/Contacts/contacts.svelte
+++ src/routes/Contacts/contacts.svelte
@@ -1,5 +1,26 @@
 <script>
 	// Add any reactive declarations or component logic here if needed
+
+	let api = import.meta.env.VITE_RESEND_EMAIL_URL;
+
+	let name = $state('');
+	let email = $state('');
+	let message = $state('');
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+		console.log('Sending email');
+
+		fetch(`${api}/send/${email}/${message}/${name}`)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				alert('Email sent successfully');
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
 </script>
 
 <section class="">
@@ -7,7 +28,7 @@
 	<div class="row align-items-center g-4">
 		<!-- Form Area -->
 		<div class="col-lg-8">
-			<form>
+			<form onsubmit={sendEmail}>
 				<div class="col-lg-10 mx-auto">
 					<div class="text-white">
 						<h1 class="fs-2 fw-semibold lh-base">Let's connect constellations</h1>
@@ -22,12 +43,16 @@
 							<input
 								type="text"
 								placeholder="Your name"
+								bind:value={name}
+								required
 								style="background-color: #0c0c0c;color:aliceblue;"
 								class="form-control bg-opacity-25"
 							/>
 						</div>
 						<div class="col-sm-6">
 							<input
+								bind:value={email}
+								required
 								type="email"
 								style="background-color: #0c0c0c;color:aliceblue;"
 								placeholder="Email address"
@@ -38,13 +63,17 @@
 
 					<div class="mt-3">
 						<textarea
+							bind:value={message}
+							required
 							placeholder="Write message"
 							style="background-color: #0c0c0c;min-height: 200px;color:aliceblue;"
 							class="form-control"
 						></textarea>
 					</div>
 
-					<button type="submit" class="btn w-100 mt-3 text-white"> Send it to the moon </button>
+					<button type="submit" class="btn w-100 mt-3 text-white">
+						Send <i class="fa-sharp fa-solid fa-paper-plane fa-2xs"></i></button
+					>
 				</div>
 			</form>
 			<br /><br />
